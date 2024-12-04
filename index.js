06677ecd1aa35e3c2d79c1b350e86fbb10a187f5
rizwana300 <22cs074@nandhaengg.org>
2024-12-04 09:58:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-//import './assets/css/bootstrap.min.css';
-import './assets/css/font-awesome.min.css';
-import './assets/css/style.css';
-//import './assets/js/bootstrap.min.js';
+const express = require("express");
+const app = express();
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const getAllPatients = require("./routes/api/getAllPatients");
+const getPatientByID = require("./routes/api/getPatientByID");
+const createPatient = require("./routes/api/createPatient");
+const editPatientByID = require("./routes/api/editPatientByID");
+const deletePatientByID = require("./routes/api/deletePatientByID");
+const LoginRegisterRoute = require("./routes/LoginRegisterRoute.js");
+const UserRoute = require("./routes/UserRoute.js");
+const DashboardRoute = require("./routes/DashboardRoute.js");
+const PatientRoute = require("./routes/PatientRoute.js");
+const DoctorRoute = require("./routes/DoctorRoute.js");
+const AppointmentRoute = require("./routes/AppointmentRoute.js");
+const MedicineRoute = require("./routes/MedicineRoute.js");
+const PrescriptionRoute = require("./routes/PrescriptionRoute.js");
+const InvoiceRoute = require("./routes/InvoiceRoute.js");
+const ProfileRoute = require("./routes/ProfileRoute.js");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGOCONNECTION, { useNewUrlParser: true });
+
+
+app.listen(process.env.PORT, () => {
+    console.log("App listening on port " + process.env.PORT);
+})
+
+app.use(LoginRegisterRoute);
+app.use(DashboardRoute);
+app.use(UserRoute);
+app.use(PatientRoute);
+app.use(DoctorRoute);
+app.use(AppointmentRoute);
+app.use(MedicineRoute);
+app.use(PrescriptionRoute);
+app.use(InvoiceRoute);
+app.use(ProfileRoute);
+
+
+
+// // API that get all patients
+// app.get('/patients', getAllPatients);
+
+// //API that gets a patient by ID
+// app.get('/patients/:id', getPatientByID);
+
+// //API for adding a patient
+// app.post('/patients', createPatient);
+
+// //API for editting a details of the patient by ID 
+// app.put('/patients/:id', editPatientByID);
+
+// //API for deleting a  patient by ID
+// app.delete('/patients/:id', deletePatientByID);
+app.use('/api/paypal', require('./routes/api/paypal'));
+
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
